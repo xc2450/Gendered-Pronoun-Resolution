dd14df6c6077f54bbd70aa675187f101f29d6c44
xc2450 <xc2450@columbia.edu>
2019-04-23 20:37:58 +0000

    modified scripts

--- feature_extraction.py
+++ feature_extraction.py
@@ -84,6 +84,35 @@ val_dist_df.to_csv('val_dist_df.csv', index=False)
 train_dist_df = extract_dist_features(train_df)
 train_dist_df.to_csv('train_dist_df.csv', index=False)
 
+
+def get_surrounding_emb(start, end, features):
+    emb = np.zeros(6144)
+    # get prev 2 and next 2 embeddings
+    prev_2 = start - 2
+    if prev_2 < 0:
+        prev_2 = 0
+    next_2 = end + 2
+    if next_2 > len(features) - 1:
+        next_2 = len(features) - 1
+    emb[:1024] = np.asarray(features[prev_2]["layers"][0]['values'])
+    emb[1024:2048] = np.asarray(features[prev_2+1]["layers"][0]['values'])
+    emb[2048:3072] = np.asarray(features[next_2]["layers"][0]['values'])
+    emb[3072:4096] = np.asarray(features[next_2-1]["layers"][0]['values'])
+    # get average of prev 5 and next 5 embeddings
+    prev_5 = start - 5
+    if prev_5 < 0:
+        prev_5 = 0
+    next_5 = end + 5
+    if next_5 > len(features) - 1:
+        next_5 = len(features) - 1
+    for i in range(prev_5, start):
+        emb[4096:5120] += np.asarray(features[i]["layers"][0]['values'])
+    emb[4096:5120] /= start-prev_5  
+    for i in range(end, next_5):
+        emb[5120:6144] += np.asarray(features[i]["layers"][0]['values'])
+    emb[5120:6144] /= next_5-end  
+    return emb
+
 def count_char(text, offset):   
     count = 0
     for pos in range(offset):
@@ -103,6 +132,7 @@ def count_token_length_special(token):
         if token[i] not in special_token: count+=1
     return count
 
+
 def embed_by_bert(df):    
     '''
     Runs a forward propagation of BERT on input text, extracting contextual word embeddings
@@ -116,7 +146,8 @@ def embed_by_bert(df):
     '''
     text = df['Text']
     text.to_csv('input.txt', index=False, header=False)
-    # run BERT on the input text
+# run BERT base on the input text
+
     os.system("python3 extract_features.py \
                --input_file=input.txt \
                --output_file=output.jsonl \
@@ -126,15 +157,27 @@ def embed_by_bert(df):
                --layers=-1 \
                --max_seq_length=256 \
                --batch_size=8")
+
+# Run BERT large to extract embedding
+
+#     os.system("python3 extract_features.py \
+#                --input_file=input.txt \
+#                --output_file=output.jsonl \
+#                --vocab_file=uncased_L-24_H-1024_A-16/vocab.txt \
+#                --bert_config_file=uncased_L-24_H-1024_A-16/bert_config.json \
+#                --init_checkpoint=uncased_L-24_H-1024_A-16/bert_model.ckpt \
+#                --layers=-1 \
+#                --max_seq_length=256 \
+#                --batch_size=8")
     
     bert_output = pd.read_json("output.jsonl", lines = True)
     bert_output.head()
     
+    features = bert_output.loc[0, "features"]
     os.system("rm input.txt")
-    os.system("rm output.jsonl")
-    
+    os.system("rm output.json1")
     index = df.index
-    columns = ["emb_A", "emb_B", "emb_P", "label"]
+    columns = ["emb_A", "emb_A_s", "emb_B", "emb_B_s", "emb_P","emb_P_s", "label"]
     emb = pd.DataFrame(index = index, columns = columns)
     emb.index.name = "ID"
     
@@ -146,37 +189,52 @@ def embed_by_bert(df):
         B_char_start = count_char(df.loc[i, 'Text'], df.loc[i, 'B-offset'])
         A_length = candidate_length(df.loc[i, 'A'])
         B_length = candidate_length(df.loc[i, 'B'])
-        
-        emb_A = np.zeros(768)
-        emb_B = np.zeros(768)
-        emb_P = np.zeros(768)
+            
+        emb_A = np.zeros(1024)
+        emb_B = np.zeros(1024)
+        emb_P = np.zeros(1024)
         
         char_count = 0
         cnt_A, cnt_B = 0, 0
+        start_A, end_A = -1,-1
+        start_B, end_B = -1,-1
+        index_P = -1
         
         for j in range(2, len(features)):
             token = features[j]["token"]
             token_length = count_token_length_special(token)
             if char_count == P_char_start:
                 emb_P += np.asarray(features[j]["layers"][0]['values']) 
+                index_P = j
             if char_count in range(A_char_start, A_char_start+A_length):
                 emb_A += np.asarray(features[j]["layers"][0]['values'])
                 cnt_A += 1
+                if start_A == -1:
+                    start_A = j
+                end_A = j
             if char_count in range(B_char_start, B_char_start+B_length):
                 emb_B += np.asarray(features[j]["layers"][0]['values'])
-                cnt_B += 1                
+                cnt_B += 1   
+                if start_B == -1:
+                    start_B = j
+                end_B = j
             char_count += token_length
         
         emb_A /= cnt_A
         emb_B /= cnt_B
         
+        emb_A_s = get_surrounding_emb(start_A, end_A, features)
+        emb_B_s = get_surrounding_emb(start_B, end_B, features)
+        emb_P_s = get_surrounding_emb(index_P, index_P, features)
+        
+        # Get label
         label = "Neither"
         if (df.loc[i,"A-coref"] == True):
             label = "A"
         if (df.loc[i,"B-coref"] == True):
             label = "B"
 
-        emb.iloc[i] = [emb_A, emb_B, emb_P, label]
+        emb.iloc[i] = [emb_A,emb_A_s, emb_B, emb_B_s, emb_P, emb_P_s, label]
         
     return emb    
 
--- train_model.py
+++ train_model.py
@@ -11,6 +11,14 @@ import tokenization
 import tensorflow as tf
 import spacy
 nlp = spacy.load('en_core_web_lg')
+from keras.layers import *
+import keras.backend as K
+from keras.models import *
+import keras
+from keras import optimizers
+from keras import callbacks
+from IPython.display import SVG
+from keras.utils.vis_utils import model_to_dot
 
 test_dist_df = pd.read_csv('test_dist_df.csv')
 val_dist_df = pd.read_csv('val_dist_df.csv')
@@ -22,52 +30,65 @@ test_df  = pd.read_table('gap-coreference/gap-development.tsv')
 train_df = pd.read_table('gap-coreference/gap-test.tsv')
 val_df   = pd.read_table('gap-coreference/gap-validation.tsv')
 
-from keras.layers import *
-import keras.backend as K
-from keras.models import *
-import keras
-from keras import optimizers
-from keras import callbacks
-from IPython.display import SVG
-from keras.utils.vis_utils import model_to_dot
 
 class End2End_NCR():
     
-    def __init__(self, word_input_shape, dist_shape, embed_dim=20): 
+    def __init__(self, word_input_shape,context_input_shape, dist_shape, embed_dim=20): 
         
         self.word_input_shape = word_input_shape
         self.dist_shape   = dist_shape
+        self.context_input_shape = context_input_shape
         self.embed_dim    = embed_dim
         self.buckets      = [1, 2, 3, 4, 5, 8, 16, 32, 64] 
-        self.hidden_dim   = 150
+        self.hidden_dim1  = 150
+        self.hidden_dim2 = 300
+        self.hidden_dim3 = 150
         
     def build(self):
         
         A, B, P = Input((self.word_input_shape,)), Input((self.word_input_shape,)), Input((self.word_input_shape,))
+        A_s, B_s, P_s = Input((self.context_input_shape,)), Input((self.context_input_shape,)), Input((self.context_input_shape,))
         dist1, dist2 = Input((self.dist_shape,)), Input((self.dist_shape,))
         inputs = [A, B, P]
         dist_inputs = [dist1, dist2]
-        
+        inputs_s = [A_s, B_s, P_s]
         self.dist_embed = Embedding(len(self.buckets)+1, self.embed_dim)
-        self.ffnn       = Sequential([Dense(self.hidden_dim, use_bias=True),
+        self.ffnn1       = Sequential([Dense(self.hidden_dim1, use_bias=True),
                                      Activation('relu'),
-                                     Dropout(rate=0.2, seed = 7),
+                                     Dropout(rate=0.2),
                                      Dense(1, activation='linear')])
+        self.ffnn2       = Sequential([Dense(self.hidden_dim2, use_bias=True),
+                                     Activation('relu'),
+                                     Dropout(rate=0.5),
+                                      Dense(self.hidden_dim3, use_bias=True),
+                                     Activation('relu'),
+                                     Dropout(rate=0.2)])
+
         
+        context_embeds = [self.ffnn2(A_s), self.ffnn2(B_s), self.ffnn2(P_s)]
+        word_embeds = [Concatenate(axis=-1)([inputs[0],context_embeds[0]]), 
+                       Concatenate(axis=-1)([inputs[1],context_embeds[1]]), 
+                       Concatenate(axis=-1)([inputs[2],context_embeds[2]])]
         dist_embeds = [self.dist_embed(dist) for dist in dist_inputs]
         dist_embeds = [Flatten()(dist_embed) for dist_embed in dist_embeds]
-        PA = Multiply()([inputs[0], inputs[2]])
-        PB = Multiply()([inputs[1], inputs[2]])
-        PA = Concatenate(axis=-1)([P, A, PA, dist_embeds[0]])
-        PB = Concatenate(axis=-1)([P, B, PB, dist_embeds[1]])
-        PA_score = self.ffnn(PA)
-        PB_score = self.ffnn(PB)
+
+        PA = Multiply()([word_embeds[0], word_embeds[2]])
+        PB = Multiply()([word_embeds[1], word_embeds[2]])
+
+        PA = Concatenate(axis=-1)([P, A, PA, context_embeds[0], context_embeds[2],dist_embeds[0]])
+        PB = Concatenate(axis=-1)([P, B, PB, context_embeds[1], context_embeds[2],dist_embeds[1]])
+        PA_score = self.ffnn1(PA)
+        PB_score = self.ffnn1(PB)
+
         score_e  = Lambda(lambda x: K.zeros_like(x))(PB_score)
+        
+        #Final Output
         output = Concatenate(axis=-1)([PA_score, PB_score, score_e]) 
         output = Activation('softmax')(output)        
-        model = Model(inputs+dist_inputs, output)
+        model = Model(inputs+inputs_s+dist_inputs, output)
         
         return model
+
 
 def create_input(embed_df, dist_df):
     
@@ -75,12 +96,15 @@ def create_input(embed_df, dist_df):
     all_P, all_A, all_B = [] ,[] ,[]
     all_label = []
     all_dist_PA, all_dist_PB = [], []
-    
+    all_P_s, all_A_s, all_B_s = [], [], []
     for i in tqdm(range(len(embed_df))):
         
         all_P.append(embed_df.loc[i, "emb_P"])
         all_A.append(embed_df.loc[i, "emb_A"])
         all_B.append(embed_df.loc[i, "emb_B"])
+        all_P_s.append(embed_df.loc[i, "emb_P_s"])
+        all_A_s.append(embed_df.loc[i, "emb_A_s"])
+        all_B_s.append(embed_df.loc[i, "emb_B_s"])
         all_dist_PA.append(dist_df.loc[i, "D_PA"])
         all_dist_PB.append(dist_df.loc[i, "D_PB"])
         label = embed_df.loc[i, "label"]
@@ -92,6 +116,7 @@ def create_input(embed_df, dist_df):
             all_label.append(2)
     
     return [np.asarray(all_A), np.asarray(all_B), np.asarray(all_P),
+            np.asarray(all_A_s), np.asarray(all_B_s), np.asarray(all_P_s),
             np.expand_dims(np.asarray(all_dist_PA),axis=1),
             np.expand_dims(np.asarray(all_dist_PB),axis=1)],all_label
 
@@ -105,66 +130,40 @@ X_test, y_test = create_input(test_emb, test_dist_df)
 
 min_loss = 1.0
 best_model = 0
+
 # Use Kfold to get best model
 
 from sklearn.model_selection import KFold
 n_fold = 5
 kfold = KFold(n_splits=n_fold, shuffle=True, random_state=3)
 for fold_n, (train_index, valid_index) in enumerate(kfold.split(X_train[0])):
-    
-    X_tr  = [inputs[train_index] for inputs in X_train]
-    X_val = [inputs[valid_index] for inputs in X_train]
-    y_tr  = np.asarray(y_train)[train_index]
-    y_val = np.asarray(y_train)[valid_index]
-    
-    model = End2End_NCR(word_input_shape=X_train[0].shape[1], dist_shape=X_train[3].shape[1]).build()
-    model.compile(optimizer=optimizers.Adam(lr=0.001), loss="sparse_categorical_crossentropy")
-    file_path = "best_model_{}.hdf5".format(fold_n+1)
-    check_point = callbacks.ModelCheckpoint(file_path, monitor = "val_loss", verbose = 0, save_best_only = True, mode = "min")
-    early_stop = callbacks.EarlyStopping(monitor = "val_loss", mode = "min", patience=100)
-    hist = model.fit(X_tr, y_tr, batch_size=128, epochs=1000, validation_data=(X_val, y_val), verbose=0,
-              shuffle=True, callbacks = [check_point, early_stop])
-    
-    if min(hist.history['val_loss']) < min_loss:
-        min_loss = min(hist.history['val_loss'])
-        best_model = fold_n + 1
+    if fold_n == 3:
+        X_tr  = [inputs[train_index] for inputs in X_train]
+        X_val = [inputs[valid_index] for inputs in X_train]
+        y_tr  = np.asarray(y_train)[train_index]
+        y_val = np.asarray(y_train)[valid_index]
+        model = End2End_NCR(word_input_shape=X_train[0].shape[1], context_input_shape=X_train[3].shape[1],dist_shape=X_train[6].shape[1]).build()
+        model.compile(optimizer=optimizers.Adam(lr=0.001), loss="sparse_categorical_crossentropy")
+        file_path = "best_model_{}.hdf5".format(fold_n+1)
+        check_point = callbacks.ModelCheckpoint(file_path, monitor = "val_loss", verbose = 0, save_best_only = True, mode = "min")
+        early_stop = callbacks.EarlyStopping(monitor = "val_loss", mode = "min", patience=5)
+        hist = model.fit(X_tr, y_tr, batch_size=128, epochs=1000, validation_data=(X_val, y_val), verbose=1,
+                  shuffle=True, callbacks = [check_point, early_stop])
 
-model = End2End_NCR(word_input_shape=X_train[0].shape[1], dist_shape=X_train[3].shape[1]).build()
+        if min(hist.history['val_loss']) < min_loss:
+            min_loss = min(hist.history['val_loss'])
+            best_model = fold_n + 1
+            
+model = End2End_NCR(word_input_shape=X_train[0].shape[1], context_input_shape=X_train[3].shape[1], dist_shape=X_train[6].shape[1]).build()
 model.load_weights("./best_model_{}.hdf5".format(best_model))
 pred = model.predict(x = X_test, verbose = 0)
 
-sub_df = pd.read_csv('sample_submission_stage_1.csv')
-sub_df.loc[:, 'A'] = pd.Series(pred[:, 0])
-sub_df.loc[:, 'B'] = pd.Series(pred[:, 1])
-sub_df.loc[:, 'NEITHER'] = pd.Series(pred[:, 2])
-
-
-from sklearn.metrics import log_loss
 y_one_hot = np.zeros((2000, 3))
 for i in range(len(y_test)):
     y_one_hot[i, y_test[i]] = 1
-log_loss(y_one_hot, pred)
-
-sub_df.to_csv("submission.csv", index=False)
-
+    
 from sklearn.metrics import f1_score
 y_true = np.argmax(y_one_hot, axis = 1)
 y_pred = np.argmax(pred, axis = 1)
-print("Overall F1: ", f1_score(y_true, y_pred, average = 'macro'))
+f1_score(y_true, y_pred, average = 'macro')
 
-f_pred = []
-m_pred = []
-f_true = []
-m_true = []
-for index, row in test_df.iterrows():
-    if row['Pronoun'] == 'her' or row['Pronoun'] == 'Her':
-        f_pred.append(y_pred[index])
-        f_true.append(y_true[index])
-    elif row['Pronoun'] == 'his' or row['Pronoun'] == 'His':
-        m_pred.append(y_pred[index])
-        m_true.append(y_true[index])
-female = f1_score(f_true, f_pred, average = 'macro')
-male = f1_score(m_true, m_pred, average = 'macro')
-print("Female F1: ", female)
-print("Male F1: ", male)
-print("Bias: ", female/male)
